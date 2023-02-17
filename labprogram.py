c8dd814cb3ae2204999444f9e8a3f18a3bd25c27
KenBarrett <KTB25@pitt.edu>
2023-02-17 10:29:07 -0500

    Added python program

---
+++ labprogram.py
@@ -0,0 +1,11 @@
+name = input("enter your name.")
+pineapples = input("Do you like pinaples on your pizza? Y or N")
+pineapples = pineapples.lower()
+if pineapples == 'n':
+	print(name + "is a good pers
+on and has a good taste for food.")
+if pineapples == 'y':
+	print(name + "is a terrible person and has no taste for food.")
+else:
+	print(name + "cannot properly answer a question.")
+
